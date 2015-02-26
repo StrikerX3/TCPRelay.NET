5eb501500276137d07219db60166c94b1f9192b1
StrikerX3 <ivan.rober@gmail.com>
2015-02-26 16:57:07 -0300

    Primitive build script

--- prepare-build.cmd
+++ prepare-build.cmd
@@ -10,6 +10,7 @@ xcopy TCPRelay\bin\Release\pt-BR out\x86\pt-BR /y /i
 xcopy TCPRelay\bin\Release\es-AR out\x86\es-AR /y /i
 xcopy TCPRelay\bin\Release\nl-NL out\x86\nl-NL /y /i
 copy TCPRelayCommon\readme.txt out\x86
+copy TCPRelayCommon\history.txt out\x86
 copy TCPRelayConsole\bin\Release\TCPRelayC.exe out\x86
 
 copy TCPRelay\bin\x64\Release\TCPRelay.exe out\x64
@@ -20,4 +21,5 @@ xcopy TCPRelay\bin\x64\Release\pt-BR out\x64\pt-BR /y /i
 xcopy TCPRelay\bin\x64\Release\es-AR out\x64\es-AR /y /i
 xcopy TCPRelay\bin\x64\Release\nl-NL out\x64\nl-NL /y /i
 copy TCPRelayCommon\readme.txt out\x64
+copy TCPRelayCommon\history.txt out\x64
 copy TCPRelayConsole\bin\x64\Release\TCPRelayC.exe out\x64
